bf8968eefd1aeda8f9c28943583a901042412c28
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2022-07-23 16:48:29 -0300

    crud user

--- bin/backend_shelf.dart
+++ bin/backend_shelf.dart
@@ -3,7 +3,7 @@ import 'package:shelf/shelf_io.dart' as io;
 
 void main(List<String> arguments) async {
   final handler = await startShelfModular();
-  final server = await io.serve(handler, '0.0.0.0', 0909);
+  final server = await io.serve(handler, '0.0.0.0', 0911);
 
   print('Server: ${server.address.address} : Port: ${server.port}');
 }
